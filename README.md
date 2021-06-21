90c6d6485527c46cef47a0974c9fa914026f8ed5
Audrey Bernier <au.bernier@hotmail.fr>
2021-06-21 13:17:57 +0200

    Modification of OtherData file with updated data

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Warsaw_Erasmus_Game
+# Warsaw_Erasmus_Game : how it works ?
+
+
+This is a quizz game dedicated to non-Polish people.
+There is one part with question of translation from English into Polish and a second part with geographic and cultural questions. 
+You have three choices for each question and only one is exact. 
+In total, you have to answer to 15 questions. At the end, you'll get a score reflecting your performance. 
+
+To be able to be sucessful, you need to have spent a least a few months in Poland. That is why, Erasmus students in Warsaw are the main target.
+ 
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from random import *
 import time
-#print("test")
 
-#print("Welcome to my quizz, you are my guess. \n Are you ready to find out if you are a real Erasmus in Warsaw ? \n The use of external help is absolutely forbidden (internet, friends..) \n You are ready now, then let's start the game with linguistic question!")
-#print("Disclaimer : my game doesn't take into account any accent. This is very sad.")
-#time.sleep(5)
+print("Welcome to my quizz, you are my guess. \n Are you ready to find out if you are a real Erasmus in Warsaw ? \n The use of external help is absolutely forbidden (internet, friends..) \n You are ready now, then let's start the game with linguistic questions!")
+time.sleep(7)
 
 df = pd.read_csv("LanguageData.csv", sep=";",encoding='utf-8')
 df2 = pd.read_csv("OtherData.csv", sep=";",encoding='utf-8')
@@ -33,7 +31,7 @@ number_of_correct_answer = 1
 number_of_the_question = 1
 
 for i in Sub_category:
-    print(f"Question n°{number_of_the_question} // {i} Category")
+    print(f"----- Question n°{number_of_the_question} // {i} Category")
     time.sleep(1)
     df_i= df[df['Sub category'] == i]
     a = randint(0,df_i.shape[0]-1)
@@ -55,17 +53,21 @@ for i in Sub_category:
 
     number_of_the_question+=1
     Already_asked_questions.append(df_i['id'].iloc[a])
-    #time.sleep(1)
+    time.sleep(1)
 
 number_of_correct_lingusitic_answers = number_of_correct_answer
 
 
-loop = 5
+loop = 6
 
-# ask loop number of questions from other data
+#ask loop number of questions from other data
+print("Now, it's time for other types of questions !")
+time.sleep(3)
 for i in range(loop):
-    print(f"Question n°{number_of_the_question} // Divers ")
+    print(f"----- Question n°{number_of_the_question} // Divers ")
     a = randint(0,count_row_otherdata-1)
+    while a in Already_asked_questions:
+        a = randint(0, df2.shape[0] - 1)
     Ask_Other_Question(a,df2)
     answer = input()
 
@@ -82,21 +84,24 @@ for i in range(loop):
 
     number_of_the_question += 1
     Already_asked_questions.append(df2['id'].iloc[a])
-    #time.sleep(1)
+    time.sleep(1)
 
 number_of_question_asked = number_of_the_question - 1
 
-#time.sleep(2)
+time.sleep(2)
 print(f"The quizz is now finished.")
 print("Thank you for your try.")
-#time.sleep(2)
+time.sleep(2)
 print(f"You score is {number_of_correct_answer} out {number_of_question_asked}")
-print(f"You had {number_of_correct_lingusitic_answers} point out of {number_of_question_asked} for lingusitic part")
+if (number_of_correct_lingusitic_answers ==1) or (number_of_correct_lingusitic_answers == 0):
+    print(f"You had {number_of_correct_lingusitic_answers} point out of {number_of_question_asked} for lingusitic part")
+else :
+    print(f"You had {number_of_correct_lingusitic_answers} points out of {number_of_question_asked} for lingusitic part")
 
 if number_of_correct_answer/number_of_question_asked >= 0.60 :
     print("You are definetely an Erasmus student in Warsaw")
 elif number_of_correct_answer/number_of_question_asked >= 0.4 :
     print("Are you really an Erasmus student ? Well let's say that you missed some of the Polish culture. ")
 else:
-    print("Well, I don't think that you are an Erasmus student, maybe you are only a tourist in Warsaw ?")
+    print("Well, I don't think that you are an Erasmus student, maybe you are only a tourist in Warsaw ? Let's stay a little bit longer in Warsaw !")
 
