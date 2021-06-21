ea4feea30ea0283e6bfe5802f3b0f055949a4366
Audrey Bernier <au.bernier@hotmail.fr>
2021-06-21 19:20:44 +0200

    Add colors and emojis

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Warsaw_Erasmus_Game : how it works ?
 
 
-This is a quizz game dedicated to non-Polish people.
-There is one part with question of translation from English into Polish and a second part with geographic and cultural questions. 
+This is a quiz game dedicated to non-Polish persons.
+There is one part with question of translation from English into Polish and a second part with geographic and cultural related questions. I created databases with the help of wikipedia and a Polish-English dictionary. 
 You have three choices for each question and only one is exact. 
-In total, you have to answer to 15 questions. At the end, you'll get a score reflecting your performance. 
+In total, you have to answer to 15 questions. At the end, you get a score reflecting your performance. 
 
-To be able to be sucessful, you need to have spent a least a few months in Poland. That is why, Erasmus students in Warsaw are the main target.
- 
-
-
-
+To be able to be successful, you need to have spent a least a few months in Poland. That is why, Erasmus students in Warsaw are my main target.
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import pandas as pd
 from random import *
 import time
 
-print("Welcome to my quizz, you are my guess. \n Are you ready to find out if you are a real Erasmus in Warsaw ? \n The use of external help is absolutely forbidden (internet, friends..) \n You are ready now, then let's start the game with linguistic questions!")
+print("Welcome to my quiz, you are my guess. \n Are you ready to find out if you are a real Erasmus in Warsaw ? 🇵🇱 \n The use of external help is absolutely forbidden (internet, friends..) \n You are ready now, then let's start the game with 10 linguistic questions!")
 time.sleep(7)
+print("")
 
 df = pd.read_csv("LanguageData.csv", sep=";",encoding='utf-8')
 df2 = pd.read_csv("OtherData.csv", sep=";",encoding='utf-8')
 
 count_row_languagedata = df.shape[0]
 count_row_otherdata = df2.shape[0]
+
 
 Sub_category=["Food","Food","Everyday","Politeness","Politeness","Places","Places","Basics","Basics"]
 
@@ -20,16 +22,17 @@ def AskQuestion_SubCategory(i,a):
     print(f"b) {df_i['B'].iloc[a]}")
     print(f"c) {df_i['C'].iloc[a]}")
 
-def Ask_Other_Question(a, df):
-    print(f"{df['Questions'].iloc[a]}")
-    print(f"a) {df['A'].iloc[a]}")
-    print(f"b) {df['B'].iloc[a]}")
-    print(f"c) {df['C'].iloc[a]}")
+def Ask_Other_Question(a, df2):
+    print(f"{df2['Questions'].iloc[a]}")
+    print(f"a) {df2['A'].iloc[a]}")
+    print(f"b) {df2['B'].iloc[a]}")
+    print(f"c) {df2['C'].iloc[a]}")
 
 Already_asked_questions=[]
-number_of_correct_answer = 1
+number_of_correct_answer = 0
 number_of_the_question = 1
 
+# asking linguistic questions first
 for i in Sub_category:
     print(f"----- Question n°{number_of_the_question} // {i} Category")
     time.sleep(1)
@@ -46,25 +49,26 @@ for i in Sub_category:
         answer = input()
 
     if answer.lower()==df_i['Correct answer'].iloc[a]:
-            print("Well done")
-            number_of_correct_answer += 1
+        print("\033[32m✔ Well done\033[0m")
+        number_of_correct_answer += 1
     else:
-        print(f" Wrong answer, the correct answer is {df['Correct answer'].iloc[a]}")
+        print(f"\033[31m❌ Wrong answer, the correct answer is {df_i['Correct answer'].iloc[a]}\033[0m")
 
     number_of_the_question+=1
     Already_asked_questions.append(df_i['id'].iloc[a])
     time.sleep(1)
+    print(" ")
 
-number_of_correct_lingusitic_answers = number_of_correct_answer
-
+number_of_correct_linguistic_answers = number_of_correct_answer
 
 loop = 6
 
-#ask loop number of questions from other data
+#Other type of questions (from other data file)
 print("Now, it's time for other types of questions !")
 time.sleep(3)
+
 for i in range(loop):
-    print(f"----- Question n°{number_of_the_question} // Divers ")
+    print(f" \n ----- Question n°{number_of_the_question} // Divers ")
     a = randint(0,count_row_otherdata-1)
     while a in Already_asked_questions:
         a = randint(0, df2.shape[0] - 1)
@@ -77,31 +81,31 @@ for i in range(loop):
         answer = input()
 
     if answer.lower()==df2['Correct answer'].iloc[a]:
-        print("Well done")
+        print("\033[32m✔ Well done\033[0m")
         number_of_correct_answer += 1
     else:
-        print(f" Wrong answer, the correct answer is {df2['Correct answer'].iloc[a]}")
+        print(f"\033[31m❌ Wrong answer, the correct answer is {df2['Correct answer'].iloc[a]}\033[0m")
 
     number_of_the_question += 1
     Already_asked_questions.append(df2['id'].iloc[a])
     time.sleep(1)
+    print("")
 
 number_of_question_asked = number_of_the_question - 1
 
-time.sleep(2)
-print(f"The quizz is now finished.")
-print("Thank you for your try.")
+time.sleep(1)
+print(f"The quiz is now finished.")
+print("Thank you for your participation !")
 time.sleep(2)
 print(f"You score is {number_of_correct_answer} out {number_of_question_asked}")
-if (number_of_correct_lingusitic_answers ==1) or (number_of_correct_lingusitic_answers == 0):
-    print(f"You had {number_of_correct_lingusitic_answers} point out of {number_of_question_asked} for lingusitic part")
+if (number_of_correct_linguistic_answers ==1) or (number_of_correct_linguistic_answers == 0):
+    print(f"You had {number_of_correct_linguistic_answers} point out of 10 for the linguistic part")
 else :
-    print(f"You had {number_of_correct_lingusitic_answers} points out of {number_of_question_asked} for lingusitic part")
+    print(f"You had {number_of_correct_linguistic_answers} points out of 10 for the linguistic part")
 
 if number_of_correct_answer/number_of_question_asked >= 0.60 :
-    print("You are definetely an Erasmus student in Warsaw")
+    print("You are definitely an Erasmus student in Warsaw 🇵🇱‍🎓‍")
 elif number_of_correct_answer/number_of_question_asked >= 0.4 :
-    print("Are you really an Erasmus student ? Well let's say that you missed some of the Polish culture. ")
+    print("Are you really an Erasmus student ... ? Well let's say that you missed some of the Polish culture. ")
 else:
-    print("Well, I don't think that you are an Erasmus student, maybe you are only a tourist in Warsaw ? Let's stay a little bit longer in Warsaw !")
-
+    print("Well, I don't think that you are an Erasmus student, maybe you are only a tourist in Warsaw ? 📸 \n You should stay a little bit longer in Warsaw !")
